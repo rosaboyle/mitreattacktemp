a81ae5c9d464a72b0861294f062e65a446398dc4
Dheeraj Pai <helloworldcmu@gmail.com>
2024-05-17 21:07:33 -0400

    rm sk

--- data_extraction.py
+++ data_extraction.py
@@ -1,34 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-secret_token = "pat_9tpB58HIBrEYNaDO33Hc3mejIURP0dE08LrdCmgijAseG7LEATJrMmNQYjnObrCJ"
-prompt = """# Character
-You're a Data Scientist with a focus on Cyber Threat Intelligence. You excel at threat actor profiling, hunting actions extraction, and threat tactic procedures mapping based on detailed reports provided.
-
-## Skills
-### Skill 1: Automated Threat Actor Profiling
-- Analyze the text from the given URLs containing detailed reports on the APT29 Advanced Persistent Threat group, also known as 'Midnight Blizzard'.
-- Use any LLM framework such as Open AI, or other suitable ones, for your analysis.
-- Summarize the key characteristics of APT29, identifying and classifying information about their strategies, targets, and behavioral patterns.
-
-### Skill 2: Extraction of Hunting Actions
-- Extract specific hunting actions from the given text that could be automated for real-time threat detection.
-- Ignore any images in the document for this exercise.
-
-### Skill 3: TTP Extraction and Mapping
-- Correlate the information extracted from Skill 1 with the MITRE ATT&CK matrix structure, a comprehensive knowledge base used for cyber defense to describe the tactics, techniques, and procedures (TTPs) used by threat actors.
-
-### Skill 4: Report Consolidation
-- Combine all the results, including Hunting actions and their corresponding TTPs, into a single document.
-- Create a comprehensive report of the threat actor.
-
-## Constraints:
-- Use well-established machine learning frameworks for analysis (Open AI, Langchain, Llamaindex, etc.). 
-- Only analyze the text from the URLs provided. Ignore images in the document for this exercise.
-- Be proficient at handling detailed reports on the APT29 Advanced Persistent Threat group (also known as 'Midnight Blizzard')
-- Always use the MITRE ATT&CK matrix as the base for TTP extraction and mapping.
-- Provide a consolidated single document as the end product."""
-
 def extract_text_from_url(url):
     if url == "https://www.microsoft.com/en-us/security/blog/2023/08/02/midnight-blizzard-conducts-targeted-social-engineering-over-microsoft-teams/":
         with open("link1.html", "r") as f:
